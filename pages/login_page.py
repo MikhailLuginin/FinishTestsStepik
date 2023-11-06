1ba247592730e53cb770afe66875ee3741eb71da
MikhailLuginin <lugininm@yandex.ru>
2023-11-06 23:27:33 +0300

    added login_page

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium import webdriver
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+    def should_be_login_url(self):
+        login_url = self.browser.current_url
+        assert "login" in login_url, "Не верный url"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.ID_LOGIN), "Формы авторизации нет"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.ID_REGISTRATION), "Формы регистрации нет"
